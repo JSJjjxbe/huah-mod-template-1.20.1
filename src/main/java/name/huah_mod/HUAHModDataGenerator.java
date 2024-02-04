f1f6ba9fbb8d9e3d270a9f5b10ca6bb4509b84a0
JSJjjxbe <s831006wekrjjf@outlook.com>
2024-02-04 19:34:28 +0800

    Initial commit

---
+++ src/main/java/name/huah_mod/HUAHModDataGenerator.java
@@ -0,0 +1,11 @@
+package name.huah_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HUAHModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
